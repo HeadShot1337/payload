be826f6d439be8a4c9d07adee5dc935f5dc39232
HeadShot1337 <147238628+HeadShot1337@users.noreply.github.com>
2026-07-17 15:56:14 +0300

    Add files via upload

---
+++ UnitRemoteExecution.pas
@@ -0,0 +1,39 @@
+unit UnitRemoteExecution;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls;
+
+type
+  TForm11 = class(TForm)
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    Edit1: TEdit;
+    Button1: TButton;
+    Label1: TLabel;
+    Edit2: TEdit;
+    Label2: TLabel;
+    Button2: TButton;
+    Button3: TButton;
+    Label3: TLabel;
+    Edit3: TEdit;
+    Button4: TButton;
+    Button5: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form11: TForm11;
+
+implementation
+
+{$R *.dfm}
+
+end.
